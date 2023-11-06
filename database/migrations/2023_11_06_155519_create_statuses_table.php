b77920029aa34e1de69525b2aafe1e2e4b13b9dd
Katerina Bouchalova <bouchalovakaterina1@gmail.com>
2023-11-06 19:24:16 +0100

    some changes

---
+++ database/migrations/2023_11_06_155519_create_statuses_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statuses', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('statuses');
+    }
+};
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,7 +1,23 @@
-import React from "react"
+import React from "react";
+import '../css/style.scss';
+
 
 export default function App () {
     return (
-        <h1>Ahoj</h1>
+       <body>
+           <nav className="sidenav">
+                <div className="sidenav__content">
+                    <img src="/logos/mi6-seal.png" alt="seal" className="sidenav__image"></img>
+                    <ul className="sidenav__links">
+                        <li className="sidenav__link sidenav__link_active"><a href="#">Home</a></li>
+                        <li className="sidenav__link"><a href="#">People of interest</a></li>
+                    </ul>
+                </div>
+                <span className="sidenav__arrow"></span>
+            </nav>
+            <main>
+                <h1>Welcome to MI6</h1>
+            </main>
+       </body>
     )
 }
