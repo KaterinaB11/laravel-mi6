48ebfbcbcffc67dd14cbd702013debd5837b166c
Katerina Bouchalova <bouchalovakaterina1@gmail.com>
2023-11-06 21:57:29 +0100

    people and persondetail without styling

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import '../css/style.scss';
 import People from "./people-of-interest/People";
-
+import PersonDetail from "./people-of-interest/PersonDetail";
 
 export default function App () {
 const [sideNavClosed, setSideNavClosed] = useState(false);
@@ -11,21 +11,24 @@ const toggleSidenav = () => {
   };
 
     return (
-        <body>
-        <nav className={`sidenav ${sideNavClosed ? 'sidenav_closed' : ''}`}>
-          <div className="sidenav__content">
-            <img src="/logos/mi6-seal.png" alt="seal" className="sidenav__image" />
-            <ul className="sidenav__links">
-              <li className="sidenav__link sidenav__link_active"><a href="#">Home</a></li>
-              <li className="sidenav__link"><a href="/people">People of interest</a></li>
-            </ul>
-          </div>
-          <span className="sidenav__arrow" onClick={toggleSidenav}></span>
-        </nav>
-        <main>
-          <h1>Welcome to MI6</h1>
+     
+          <body>
+          <nav className={`sidenav ${sideNavClosed ? 'sidenav_closed' : ''}`}>
+            <div className="sidenav__content">
+              <img src="/logos/mi6-seal.png" alt="seal" className="sidenav__image" />
+              <ul className="sidenav__links">
+                <li className="sidenav__link sidenav__link_active"><a href="#">Home</a></li>
+                <li className="sidenav__link"><a href="/people">People of interest</a></li>
+              </ul>
+            </div>
+            <span className="sidenav__arrow" onClick={toggleSidenav}></span>
+          </nav>
+          <main>
+            <h1>Welcome to MI6</h1>
+          </main>
           <People/>
-        </main>
-      </body>
+          <PersonDetail/>
+        </body>
+     
     )
 }
